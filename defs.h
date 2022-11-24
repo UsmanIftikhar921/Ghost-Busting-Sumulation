aad2c1be5fd4d5114f360df797d30e3f3cbd75b6
Cylex9212 <USMANIFTIKHAR@cmail.carleton.ca>
2022-11-24 11:51:13 -0500

    Initial Code

---
+++ defs.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAX_STR            64
+#define FEAR_RATE           1
+#define MAX_FEAR          100
+#define MAX_HUNTERS         4
+#define USLEEP_TIME     50000
+#define BOREDOM_MAX        99
+
+// You may rename these types if you wish
+typedef enum { EMF, TEMPERATURE, FINGERPRINTS, SOUND } EvidenceClassType;
+typedef enum { POLTERGEIST, BANSHEE, BULLIES, PHANTOM } GhostClassType;
+
+int randInt(int, int);          // Generates a pseudorandom integer between the parameters
+float randFloat(float, float);  // Generates a pseudorandom float between the parameters
+
+void populateRooms(BuildingType*);   // Populates the building with sample data for rooms
