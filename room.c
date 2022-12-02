2068a4c6f0f51d598329f1ef863023b9457ff067
Cylex9212 <USMANIFTIKHAR@cmail.carleton.ca>
2022-12-02 17:01:42 -0500

    Update room.c

--- room.c
+++ room.c
@@ -1,11 +1,24 @@
 #include "defs.h"
 
+/*
+  Function:  initRoomList
+  Purpose:   initialize a list of rooms by setting it's head and tail to null, and it's size to 0
+       in:   a pointer to a RoomListType
+   return:   a pointer to a RoomListType that has been initialized
+*/
 void initRoomList(RoomListType * roomList){
 	roomList->head = NULL;
 	roomList->tail = NULL;
 	roomList->size = 0;
 }
 
+/*
+  Function:  initRoom
+  Purpose:   initialize a room by setting it's fields to the corresponding values provided
+       in:   a double pointer to a room
+       in:   a pointer to the name of the room
+   return:   a pointer to a RoomType with initialized contents
+*/
 void initRoom(char * name, RoomType ** room){
 	//Assign memory for the room data
 	*room = calloc(1, sizeof(RoomType));
@@ -19,7 +32,13 @@ void initRoom(char * name, RoomType ** room){
 	initHunterArray((*room) -> hunters);
 }
 
-// Adds a room to the end of a RoomListType
+/*
+  Function:  addRoom
+  Purpose:   adds a room to the room list
+       in:   a pointer to a RoomListType
+       in:   a pointer to a RoomType
+   return:   a pointer to a RoomListType with a room element inside it
+*/
 void addRoom(RoomListType * list, RoomType * room){	
 	//A temporary node to store the information of the new tail to be added
 	RoomNodeType * newNode;
@@ -46,6 +65,13 @@ void addRoom(RoomListType * list, RoomType * room){
    	}
 }
 
+/*
+  Function:  connectRooms
+  Purpose:   connects two rooms together by adding the opposing room to each other's connected rooms list
+       in:   a pointer to a RoomType for the first room
+       in:   a pointer to a RoomType for the second room
+   return:   pointers to two rooms that are connected to each other
+*/
 void connectRooms(RoomType * room1, RoomType * room2){
 	// Add Room 2 to Room 1's attached roomList
 	addRoom(room1->attached, room2);
@@ -54,6 +80,12 @@ void connectRooms(RoomType * room1, RoomType * room2){
 	addRoom(room2->attached, room1);
 }
 
+/*
+  Function:  cleanupRoom
+  Purpose:   clears all of the contents of the room
+       in:   a pointer to a RoomType
+   return:   a pointer to an empty RoomType
+*/
 void cleanupRoom(RoomType * room){
 	// Free Evidence and Room Linked List
 	cleanupEvidenceList(room -> evidence);
@@ -62,6 +94,12 @@ void cleanupRoom(RoomType * room){
 	cleanupHunterArray(room -> hunters);
 }
 
+/*
+  Function:  cleanupRoomList
+  Purpose:   clears all of the contents of the room list
+       in:   a pointer to a RoomListType
+   return:   a pointer to an empty RoomListType
+*/
 void cleanupRoomList(RoomListType * roomList){
 	// Record the size of the room
 	int roomListSize = roomList -> size;
@@ -79,6 +117,13 @@ void cleanupRoomList(RoomListType * roomList){
 	}
 }
 
+/*
+  Function:  randRoom
+  Purpose:   get a random room from the room list
+       in:   a pointer to a RoomListType
+       in:   an integer that determines whether or not the ghost can move inside the van
+   return:   a pointer to a room that has been randomly chosen
+*/
 RoomType * randRoom(RoomListType * roomList, int spawnGhostCheck) {
     if (spawnGhostCheck == C_FALSE) {
         int selectedRoom = randInt(0, roomList -> size - 1);
